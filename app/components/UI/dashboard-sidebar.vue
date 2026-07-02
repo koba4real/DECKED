60f8ebaa51c3e0e40da7de3cb18a79fc8cded7b9
koba <132523021+koba4real@users.noreply.github.com>
2026-07-02 16:22:15 +0200

    As a player, I want to navigate the app through a consistent layout #20

---
+++ app/components/UI/dashboard-sidebar.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <UDashboardSidebar
+    id="default"
+    collapsible
+    resizable
+  >
+    <template #default="{ collapsed }">
+      <UDashboardSidebarCollapse />
+
+      <SidebarDefault :collapsed="collapsed" />
+    </template>
+  </UDashboardSidebar>
+</template>
---
+++ app/components/UI/sidebar-default.vue
@@ -0,0 +1,71 @@
+<script setup lang="ts">
+import type { NavigationMenuItem } from "@nuxt/ui";
+
+defineProps<{ collapsed: boolean }>();
+
+const route = useRoute();
+const authStore = useAuthStore();
+
+const navItems = computed<NavigationMenuItem[]>(() => [
+  {
+    label: "Dashboard",
+    icon: "tabler:home",
+    to: "/dashboard",
+    active: route.path === "/dashboard",
+  },
+  {
+    label: "Leaderboard",
+    icon: "material-symbols-light:leaderboard-rounded",
+    to: "/dashboard/leaderboard",
+    active: route.path === "/dashboard/leaderboard",
+  },
+  {
+    label: "Log Game",
+    icon: "tabler:device-gamepad",
+    to: "/dashboard/log-game",
+    active: route.path === "/dashboard/log-game",
+  },
+]);
+
+const accountItems = computed<NavigationMenuItem[]>(() => [
+  {
+    label: "profile",
+    icon: "pajamas:profile",
+  },
+  {
+    label: "Logout",
+    icon: "tabler:logout",
+    onSelect: () => authStore.signOut(),
+  },
+]);
+</script>
+
+<template>
+  <UNavigationMenu
+    :collapsed="collapsed"
+    :items="navItems"
+    orientation="vertical"
+    :tooltip="true"
+  />
+
+  <UNavigationMenu
+    :collapsed="collapsed"
+    :items="accountItems"
+    orientation="vertical"
+    class="sidebar__nav--bottom"
+    :tooltip="true"
+  />
+</template>
+
+<style scoped>
+.sidebar__kbd {
+  display: flex;
+  align-items: center;
+  gap: 0.125rem;
+  margin-inline-start: auto;
+}
+
+.sidebar__nav--bottom {
+  margin-top: auto;
+}
+</style>
